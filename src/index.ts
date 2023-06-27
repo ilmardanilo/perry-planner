471cb4c9e5c2f556e23dba715478bf8c915ea632
Ilmar <ilmar.danilo@querodelivery.com>
2023-06-27 13:26:22 -0300

    refactor: improve server

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,6 @@
-import express from "express";
-import cors from "cors";
-import { PORT } from "./common/environment-consts";
-import { routes } from "./routes";
+import { PORT } from "./config/environment-consts";
+import { app } from "./config/app";
 
-const app = express();
-app.use(cors());
-app.use(routes);
-
-const server = app.listen(PORT, () => {
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+app.listen(PORT, () => {
   console.log(`listening on port ${PORT} 🚀`);
 });
-
-export { server };
