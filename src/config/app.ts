7e853168af0c264d9c8735f6396bd715586f36f6
Ilmar <ilmar.danilo@querodelivery.com>
2023-06-27 13:24:21 -0300

    feat: add app in file

---
+++ src/config/app.ts
@@ -0,0 +1,7 @@
+import { setupMiddlewares } from "./setup-middlewares";
+import { setupRoutes } from "./setup-routes";
+import express from "express";
+
+export const app = express();
+setupMiddlewares(app);
+setupRoutes(app);
