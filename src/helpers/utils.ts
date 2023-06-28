1887dc228851d461777e1c934840dc3150b7f68e
Ilmar <ilmar.danilo@querodelivery.com>
2023-06-28 13:44:25 -0300

    feat: add getMonthString in utils

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,5 +1,6 @@
 import { HttpError } from "express-openapi-validator/dist/framework/types";
 import { Response } from "express";
+import { EMonth } from "../modules/movimento/movimento.interfaces";
 
 export const handleError = (res: Response, error: any) => {
   if (error instanceof HttpError) {
@@ -17,4 +18,52 @@ export const handleError = (res: Response, error: any) => {
   }
 
   return res.status(500).json({ message: "Unexpected error!" });
+};
+
+export const getMonthString = (month: number): EMonth => {
+  let monthString: EMonth;
+
+  switch (month) {
+    case 0:
+      monthString = EMonth.JANEIRO;
+      break;
+    case 1:
+      monthString = EMonth.FEVEREIRO;
+      break;
+    case 2:
+      monthString = EMonth.MARCO;
+      break;
+    case 3:
+      monthString = EMonth.ABRIL;
+      break;
+    case 4:
+      monthString = EMonth.MAIO;
+      break;
+    case 5:
+      monthString = EMonth.JUNHO;
+      break;
+    case 6:
+      monthString = EMonth.JULHO;
+      break;
+    case 7:
+      monthString = EMonth.AGOSTO;
+      break;
+    case 8:
+      monthString = EMonth.SETEMBRO;
+      break;
+    case 9:
+      monthString = EMonth.OUTUBRO;
+      break;
+    case 10:
+      monthString = EMonth.NOVEMBRO;
+      break;
+    case 11:
+      monthString = EMonth.DEZEMBRO;
+      break;
+    default:
+      monthString = EMonth.JANEIRO;
+      break;
+  }
+
+  return monthString;
 };
